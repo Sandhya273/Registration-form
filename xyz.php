4cd85c21bc8b065eba3b890e1c3826c9fd81fb55
Sandhya273 <sandhyamadagoni02@gmail.com>
2024-12-04 19:13:27 +0530

    Add files via upload

--- xyz.php
+++ xyz.php
@@ -5,15 +5,28 @@
             Enter First Name:<br>
             <input name="first_name"><br>
             Enter Second Name:<br>
-            <input name="second_name"><br><br>
-            <button type="submit">sub</button>
+            <input name="second_name"><br>
+            <p>status:</p><select name="dropdown">
+            <option name="active">active</option>
+            <option name="inactive">inactive</option>
+            </select><br><br>
+            <button type="submit">submit</button>
         </form>
         <?php
-        if (isset($_REQUEST["first_name"]) && isset($_REQUEST["second_name"])){
-            $a=$_REQUEST["first_name"];
-            $b=$_REQUEST["second_name"];
-            echo $a;
-            echo $b;
+        if (isset($_REQUEST["first_name"]) && isset($_REQUEST["second_name"])) {
+        
+            $a = ($_REQUEST["first_name"]);
+            $b = ($_REQUEST["second_name"]);
+
+    
+            $status = 0; 
+            if (isset($_REQUEST["dropdown"]) && $_REQUEST["dropdown"] == "active") {
+                $status = 1; 
+            }
+
+            echo "First Name: " . $a . "<br>";
+            echo "Second Name: " . $b . "<br>";
+            echo "Status: " . ($status == 1 ? "Active" : "Inactive") . "<br>";
         }
         ?>
 
